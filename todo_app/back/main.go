f90c8538bd0ab7a7affee8b5ea329cf4b862221d
jakeNorman007 <jacob.a.norman91@gmail.com>
2023-11-14 23:05:00 -0500

    go packages, cleaned up Vue

--- todo_app/back/main.go
+++ todo_app/back/main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
-    "fmt"
+    "log"
+    "github.com/gin-gonic/gin"
+    "github.com/joho/godotenv"
 )
 
+func init(){
+    err := godotenv.Load()
+    
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+}
+
 func main(){
-    fmt.Println("Hello world")
+    r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
